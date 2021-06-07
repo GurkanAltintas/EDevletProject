c177a5c930086ae46a400ae6d038472c13918dc5
gurkan <qurkan.a00@gmail.com>
2021-06-07 22:06:53 +0300

    login part

---
+++ src/main/webapp/e-nabiz/src/components/login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h1> fuck u oşan</h1>
+
+            </div>
+        );
+    }
+}
+
+export default Login;

d826da615e97f8f0f6f9b9645a5ff1ded5530244
gurkan <qurkan.a00@gmail.com>
2021-06-08 23:37:17 +0300

    register - login part generated

--- src/main/webapp/e-nabiz/src/App.js
+++ src/main/webapp/e-nabiz/src/App.js
@@ -1,10 +1,25 @@
 import { Container } from "reactstrap"
 import Login from "./components/login/Login";
+import Error from "./components/error/Error";
+import Register from "./components/register/Register";
+import { BrowserRouter,Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
         <Container>
-            <Login/>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route exact component={Error}/>
+                    <Login/>
+                </Switch>
+
+
+            </BrowserRouter>
+
+
+
 
         </Container>
 
---
+++ src/main/webapp/e-nabiz/src/components/register/Register.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                <h1>register page</h1>
+
+            </div>
+        );
+    }
+}
+
+export default Register;
